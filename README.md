3020e2dc12739c57a6a33fb6d59b587b487cc13a
sahriyar eshar <princeeshar@gmail.com>
2023-12-13 17:28:01 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# task-manager
+ this is an next js to do list app
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Tasks from './Components/Tasks/Tasks'
+import { useGlobalState } from './context/globalProvider'
+
+export default function Home() {
+
+  const {tasks} = useGlobalState();
+  return (
+    <Tasks tasks={tasks} title="All Tasks" />
+  )
+}
